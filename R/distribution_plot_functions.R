80a9de391625fed0823fca7759813f7d20a4ec90
Lakhansing Pardeshi <lakhanp1@gmail.com>
2018-11-04 19:00:12 +0800

    new argument: ylim

--- R/distribution_plot_functions.R
+++ R/distribution_plot_functions.R
@@ -11,19 +11,36 @@
 #' @param colorCol column name which should be used for coloring the points
 #' @param pseudoCount A small number to be added to the values to avoid log(0) error.
 #' Default: 0.1
+#' @param ylim A two element numeric vector for Y-limits of MA plot. Default: c(-4, 4)
 #'
 #' @return a ggplot2 object which has MA plot
 #' @export
 #'
 #' @examples NA
-plot_MA_gg = function(df, s1, s2, title = "MA plot", colorCol, pseudoCount = 0.1){
+plot_MA_gg = function(df, s1, s2, title = NA, colorCol, pseudoCount = 0.1, ylim = NULL){
 
+  ## check for valid ylim
+  if(!is.null(ylim) & length(ylim) != 2 & !is.numeric(ylim)){
+    stop("wrong ylim option. Please provide ylim as numeric vector of length 2")
+  } else{
+    ylim = sort(ylim)
+  }
 
   df2 <- df %>%
     dplyr::mutate(
       M = log2(!!as.name(s1) + pseudoCount) - log2(!!as.name(s2) + pseudoCount),
       A = (log2(!!as.name(s1) + pseudoCount) + log2(!!as.name(s2) + pseudoCount)) / 2
-    )
+    ) %>%
+    {
+      if(!is.null(ylim)){
+        dplyr::mutate(.data = .,
+                      M = dplyr::if_else(M < ylim[1], true = ylim[1], false = M)) %>%
+          dplyr::mutate(M = dplyr::if_else(M > ylim[2], true = ylim[2], false = M))
+      } else{
+        .
+      }
+    }
+
 
   ## if the column for color does not exists, create new column with value "genes"
   ## this also means no group wise coloring
@@ -37,20 +54,21 @@ plot_MA_gg = function(df, s1, s2, title = "MA plot", colorCol, pseudoCount = 0.1
     df2[[colorCol]] <- as.character(df2[[colorCol]])
   }
 
+  if(is.na(title)){
+    title <- paste("MA plot: ", s1, "/", s2)
+  }
+
   maPlot <- ggplot() +
     geom_point(data = df2, mapping = aes(x = A, y = M, color = !!as.name(colorCol))) +
+    geom_hline(yintercept = 0) +
+    geom_hline(yintercept = 1, linetype = 2, alpha = 0.5) +
+    geom_hline(yintercept = -1, linetype = 2, alpha = 0.5) +
     ggtitle(title) +
     xlab("Mean expression (log2)") +
     ylab("log2(fold change)") +
     theme_bw() +
-    theme(plot.title = element_text(hjust = 0.5, size = 20, face = "bold"),
-          axis.text = element_text(size = 20),
-          axis.title = element_text(size = 20),
-          legend.text = element_text(size = 20),
-          legend.title = element_text(size = 20, face = "bold"),
-          legend.box.background = element_rect(colour = "black"),
-          legend.box.margin = margin(1, 1, 1, 1)
-    )
+    theme_classic()
+
 
 
   return(maPlot)
