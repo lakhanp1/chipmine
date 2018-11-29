93ca2a20a97284c95721c205c9bd251d263ad10b
Lakhansing Pardeshi <lakhanp1@gmail.com>
2018-11-29 14:15:34 +0800

    changes in average profile plot functions
    verify whether all the matrices have same dimensions
    automatic x axis label configuration for average profile plot

--- R/average_profile_functions.R
+++ R/average_profile_functions.R
@@ -18,11 +18,19 @@
 geneset_average_profile <- function(exptInfo, profileMats, genes, cluster = "group_A"){
 
   meanProfile <- list()
+  checkList <- c(attributes(profileMats[[1]])$dim, attributes(profileMats[[1]])$extend)
 
+  # i <- 1
   ## calculate mean profile for each matrix
   for (i in 1:nrow(exptInfo)) {
-    mat <- profileMats[[ exptInfo$sampleId[i] ]][genes, ]
+    mat <- profileMats[[ exptInfo$sampleId[i] ]]
 
+    ## make sure that all the matrices have same dimensions and extension
+    if(! all(checkList == c(attributes(mat)$dim, attributes(mat)$extend))){
+      stop("Error: matrix are of not same dimentions")
+    }
+
+    mat <- mat[genes, ]
 
     sdCol <- paste(exptInfo$sampleId[i], "_sd", sep = "")
 
@@ -33,13 +41,19 @@ geneset_average_profile <- function(exptInfo, profileMats, genes, cluster = "gro
                                                       attributes(mat)$target_index,
                                                       attributes(mat)$downstream_index)
 
+    names(meanProfile[[ sdCol ]]) <- c(attributes(mat)$upstream_index,
+                                       attributes(mat)$target_index,
+                                       attributes(mat)$downstream_index)
+
     cat("Calculated mean profile for sample", exptInfo$sampleId[i], "\n")
   }
 
-  meanProfileDf <- data.table::as.data.table(meanProfile) %>%
-    tibble::rownames_to_column(var = "bin")
+  meanProfileDf <- data.table::as.data.table(meanProfile)
 
-  meanProfileDf$bin <- as.numeric(meanProfileDf$bin)
+  meanProfileDf$bin <- c(attributes(profileMats[[1]])$upstream_index,
+                         attributes(profileMats[[1]])$target_index,
+                         attributes(profileMats[[1]])$downstream_index)
+
 
   grp1Cols <- exptInfo$sampleId
   grp2Cols <- paste(exptInfo$sampleId, "_sd", sep = "")
@@ -87,64 +101,59 @@ draw_avg_profile_plot <- function(exptInfo,
                                   lineColors = NULL,
                                   lineShape = NULL){
 
-  meanProfile <- list()
 
-  if(is.null(lineColors)){
-    lineColors <- pkg.env$customColors[1:nrow(exptInfo)]
+  plotDf <- geneset_average_profile(exptInfo = exptInfo, profileMats = profileMats, genes = genes)
+
+  ## decide the axis labels
+  axisBrk <- NULL
+  axisLab <- NULL
+  if(attributes(profileMats[[1]])$target_is_single_point){
+    axisBrk <- c(
+      attributes(profileMats[[1]])$upstream_index[1],
+      attributes(profileMats[[1]])$target_index[1],
+      tail(attributes(profileMats[[1]])$downstream_index, 1)
+    )
+
+    axisLab <- c(
+      -attributes(profileMats[[1]])$extend[1],
+      attributes(profileMats[[1]])$target_name,
+      attributes(profileMats[[1]])$extend[2]
+    )
+
+    targetEnd <- axisBrk[2] + 1
+
+  } else if(! attributes(profileMats[[1]])$target_is_single_point){
+    axisBrk <- c(
+      attributes(profileMats[[1]])$upstream_index[1],
+      attributes(profileMats[[1]])$target_index[1],
+      tail(attributes(profileMats[[1]])$target_index, 1),
+      tail(attributes(profileMats[[1]])$downstream_index, 1)
+    )
+
+    axisLab <- c(
+      -attributes(profileMats[[1]])$extend[1],
+      "START", "END",
+      attributes(profileMats[[1]])$extend[2]
+    )
+
+    targetEnd <- axisBrk[3]
+
   }
-
-  if(is.null(lineShape)){
-    lineShape <- structure(rep("a", nrow(exptInfo)), names = exptInfo$sampleId)
-  }
-
-  ## calculate mean profile for each matrix
-  for (i in 1:nrow(exptInfo)) {
-    mat <- profileMats[[ exptInfo$sampleId[i] ]][genes, ]
-
-
-    sdCol <- paste(exptInfo$sampleId[i], "_sd", sep = "")
-
-    meanProfile[[ exptInfo$sampleId[i] ]] <- apply(mat, MARGIN = 2, mean)
-    meanProfile[[ sdCol ]] <- apply(mat, MARGIN = 2, sd)
-
-    names(meanProfile[[ exptInfo$sampleId[i] ]]) <- c(attributes(mat)$upstream_index,
-                                                      attributes(mat)$target_index,
-                                                      attributes(mat)$downstream_index)
-
-    cat("Calculated mean profile for sample", exptInfo$sampleId[i], "\n")
-  }
-
-
-  meanProfileDf <- data.table::as.data.table(meanProfile) %>% tibble::rownames_to_column(var = "bin")
-  meanProfileDf$bin <- as.numeric(meanProfileDf$bin)
-
-  grp1Cols <- exptInfo$sampleId
-  grp2Cols <- paste(exptInfo$sampleId, "_sd", sep = "")
-
-  plotDf <- data.table::melt(data = meanProfileDf,
-                            id.vars = c("bin"),
-                            measure.vars = list(grp1Cols, grp2Cols),
-                            value.name = c("mean", "sd"),
-                            variable.name = "sample"
-  )
-
-  levels(plotDf$sample) <- exptInfo$sampleId
-
 
   p <- ggplot2::ggplot(data = plotDf) +
     geom_line(mapping = aes(x = bin, y = mean, group = sample, color = sample, linetype = sample),
-              size = 1) +
-    geom_segment(mapping = aes(x = 200, y = 0, xend = 400, yend = 0), size = 10, lineend = "butt", color = "grey70") +
+              size = 0.8, alpha = 0.8) +
     geom_hline(yintercept = 0, size = 2, color = "grey70") +
-    annotate(geom = "text", x = 230, y = 0, label = "Scaled gene body", hjust = 0, vjust = 0.3) +
+    geom_segment(mapping = aes(x = axisBrk[2], y = 0, xend = targetEnd, yend = 0),
+                 size = 10, lineend = "butt", color = "grey50") +
+    # annotate(geom = "text", x = 230, y = 0, label = "Scaled gene body", hjust = 0, vjust = 0.3) +
     scale_color_manual(values = lineColors) +
     scale_linetype_manual(values = lineShape) +
-    scale_x_continuous(breaks = c(0, 100, 200, 400, 500),
-                       labels = c("-2kb", "-1kb", "ATG", "STOP", "+1kb")) +
+    scale_x_continuous(breaks = axisBrk, labels = axisLab) +
     ylab("Read coverage") +
     theme_bw() +
     theme(plot.title = element_text(hjust = 0.5, size = 14, face = "bold"),
-          axis.text.x = element_text(size = 13),
+          axis.text.x = element_text(size = 13, angle = 90),
           axis.text.y = element_text(size = 15),
           axis.title.x = element_blank(),
           axis.title.y = element_text(face = "bold"),
