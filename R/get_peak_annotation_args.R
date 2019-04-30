8148a4556230e4c7ab5b1bd66b871408c871fc05
Lakhansing Pardeshi <lakhanp1@gmail.com>
2019-04-30 18:59:14 +0800

    peak type annotation colors updated as per new annotation

--- R/get_peak_annotation_args.R
+++ R/get_peak_annotation_args.R
@@ -3,44 +3,42 @@
 ## peak type color and heatmap legend
 #' Get peak heatmap annotation
 #'
-#' @param tssCol column name of the TSS region peaks
-#' @param tesCol column name of the TES region peaks
+#' @param anName Annotation name
 #'
 #' @return A list of two elements: peak type color and peak type legend
 #' @export
 #'
 #' @examples NA
-get_peak_annotation_args = function(tssCol, tesCol){
+get_peak_annotation_args = function(anName){
   ## peak types
-  peakTypes = c("upstream", "overlapStart", "insideOverlapStart", "includeFeature", "insideOverlapStartOverlapEnd", "inside", "insideOverlapEnd", "overlapEnd", "pseudo_upstream", "pseudo_overlapEnd", "pseudo_insideOverlapEnd", "pseudo_inside")
-
-
-  peakColors = c(RColorBrewer::brewer.pal(8, "Paired"), "black", "black", "black", "black")
-
-  peakTypeCol = sapply(
-    X = c(tssCol, tesCol),
-    FUN = function(x){
-      structure(.Data = peakColors,
-                names = peakTypes)},
-    USE.NAMES = T,
-    simplify = F
-  )
+  peakTypeCol = c("include_tx" = "#e41a1c", "include_CDS" = "#e41a1c",
+                "5UTR" = "#377eb8", "CDS_start" = "#377eb8", "tx_start" = "#377eb8",
+                "3UTR" = "#4daf4a", "tx_end" = "#4daf4a", "CDS_end" = "#4daf4a",
+                "inside_tx" = "#984ea3", "inside_CDS" = "#984ea3",
+                "promoter" = "#ff7f00", "upstream" = "#ff7f00",
+                "pseudo_upstream" = "black", "pseudo_upstream" = "black")
 
   ## legend for all the annotations
   peakTypeLgd = Legend(title = "\nPeak type",
-                       at = peakTypes,
+                       at = names(peakTypeCol),
                        type = "points",
                        pch = 15,
                        size = unit(5, "mm"),
                        grid_height = unit(5, "mm"),
                        grid_width = unit(5, "mm"),
-                       legend_gp = gpar(col = peakColors),
+                       legend_gp = gpar(col = peakTypeCol),
                        title_gp = gpar(fontsize = 12, fontface = "bold"),
                        labels_gp = gpar(fontsize = 8)
   )
 
+  ## draw the legend
+  # pushViewport(viewport(width = 0.9, height = 0.9))
+  # grid.rect()
+  # draw(peakTypeLgd, x = unit(0.5, "npc"), y = unit(0.5, "npc"))
+  # popViewport()
+
   return(list(
-    col = peakTypeCol,
+    col = list(peakTypeCol) %>% purrr::set_names(nm = anName),
     lgd = peakTypeLgd
   ))
 
