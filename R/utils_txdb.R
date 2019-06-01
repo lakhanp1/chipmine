a78b3c19f1494e146481aa9b65df4b024a475bdb
Lakhansing Pardeshi <lakhanp1@gmail.com>
2019-06-01 19:24:16 +0800

    all annotation feature functions support tx argument
    optionally tx argument can be provided to select the features for transcripts of interest. tx should be the vector of internal tx_id

--- R/utils_txdb.R
+++ R/utils_txdb.R
@@ -16,6 +16,8 @@ get_txdb_transcripts_gr <- function(txdb, excludeType = NULL, tx = NULL){
       dplyr::mutate(TXID = as.character(TXID)) %>%
       dplyr::rename(geneId = GENEID, txName = TXNAME, txType = TXTYPE)
 
+    assign(x = "txToGene", value = txToGene, envir = txdbEnv)
+
     ## decide which transcript types to select for the annotation
     allTxTypes <- unique(txToGene$txType)
     selectType <- allTxTypes[which(!allTxTypes %in% excludeType)]
@@ -43,40 +45,64 @@ get_txdb_transcripts_gr <- function(txdb, excludeType = NULL, tx = NULL){
 ##################################################################################
 
 
-get_txdb_fiveUtr_grl <- function(txdb){
+get_txdb_fiveUtr_gr <- function(txdb, tx = NULL){
 
-  if(exists("fiveUtrGrl", envir=txdbEnv, inherits=FALSE)) {
-    fiveUtrGrl <- get(x = "fiveUtrGrl", envir = txdbEnv)
+  if(exists("fiveUtrGr", envir=txdbEnv, inherits=FALSE)) {
+    fiveUtrGr <- get(x = "fiveUtrGr", envir = txdbEnv)
+
   } else{
     fiveUtrGrl <- GenomicFeatures::fiveUTRsByTranscript(txdb)
-    assign(x = "fiveUtrGrl", value = fiveUtrGrl, envir = txdbEnv)
+
+    if(!is.null(tx)){
+      fiveUtrGrl <- fiveUtrGrl[names(fiveUtrGrl) %in% tx]
+    }
+
+    ## remember to combine the multi-exon UTRs from UTR GRangesList
+    fiveUtrGr <- unlist(range(fiveUtrGrl))
+    mcols(fiveUtrGr)$tx_id <- names(fiveUtrGr)
+    assign(x = "fiveUtrGr", value = fiveUtrGr, envir = txdbEnv)
   }
 
-  return(fiveUtrGrl)
+  return(fiveUtrGr)
 }
 
 ##################################################################################
 
-get_txdb_threeUtr_grl <- function(txdb){
-  if(exists("threeUtrGrl", envir=txdbEnv, inherits=FALSE)) {
-    threeUtrGrl <- get(x = "threeUtrGrl", envir = txdbEnv)
+get_txdb_threeUtr_gr <- function(txdb, tx = NULL){
+  if(exists("threeUtrGr", envir=txdbEnv, inherits=FALSE)) {
+    threeUtrGr <- get(x = "threeUtrGr", envir = txdbEnv)
+
   } else{
     threeUtrGrl <- GenomicFeatures::threeUTRsByTranscript(txdb)
-    assign(x = "threeUtrGrl", value = threeUtrGrl, envir = txdbEnv)
+
+    if(!is.null(tx)){
+      threeUtrGrl <- threeUtrGrl[names(threeUtrGrl) %in% tx]
+    }
+
+    ## remember to combine the multi-exon UTRs from UTR GRangesList
+    threeUtrGr <- unlist(range(threeUtrGrl))
+    mcols(threeUtrGr)$tx_id <- names(threeUtrGr)
+    assign(x = "threeUtrGr", value = threeUtrGr, envir = txdbEnv)
   }
 
-  return(threeUtrGrl)
+  return(threeUtrGr)
 }
 
 
 ##################################################################################
 
 
-get_txdb_exons_gr <- function(txdb){
+get_txdb_exons_gr <- function(txdb, tx = NULL){
   if(exists("exonsGr", envir=txdbEnv, inherits=FALSE)) {
     exonsGr <- get(x = "exonsGr", envir = txdbEnv)
   } else{
-    exonsGr <- unlist(GenomicFeatures::exonsBy(x = txdb, by = "tx"))
+    exonsGrl <- GenomicFeatures::exonsBy(x = txdb, by = "tx")
+
+    if(!is.null(tx)){
+      exonsGrl <- exonsGrl[names(exonsGrl) %in% tx]
+    }
+
+    exonsGr <- unlist(exonsGrl)
     mcols(exonsGr)$tx_id <- names(exonsGr)
     assign(x = "exonsGr", value = exonsGr, envir = txdbEnv)
   }
@@ -88,11 +114,17 @@ get_txdb_exons_gr <- function(txdb){
 ##################################################################################
 
 
-get_txdb_introns_gr <- function(txdb){
+get_txdb_introns_gr <- function(txdb, tx = NULL){
   if(exists("intronsGr", envir=txdbEnv, inherits=FALSE)) {
     intronsGr <- get(x = "intronsGr", envir = txdbEnv)
   } else{
-    intronsGr <- unlist(GenomicFeatures::intronsByTranscript(x = txdb))
+    intronsGrl <- GenomicFeatures::intronsByTranscript(x = txdb)
+
+    if(!is.null(tx)){
+      intronsGrl <- intronsGrl[names(intronsGrl) %in% tx]
+    }
+
+    intronsGr <- unlist(intronsGrl)
     mcols(intronsGr)$tx_id <- names(intronsGr)
     assign(x = "intronsGr", value = intronsGr, envir = txdbEnv)
   }
@@ -102,4 +134,5 @@ get_txdb_introns_gr <- function(txdb){
 
 
 ##################################################################################
+
 
