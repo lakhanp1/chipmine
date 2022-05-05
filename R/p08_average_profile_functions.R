df2fd5d8cbd4750b0ad7d7f419c29f4089bc0852
Lakhansing Pardeshi <lakhanp1@gmail.com>
2022-05-05 22:13:17 +0200

    bug fix: sample to sampleId

--- R/p08_average_profile_functions.R
+++ R/p08_average_profile_functions.R
@@ -16,36 +16,36 @@
 #'
 #' @examples NULL
 geneset_average_profile <- function(exptInfo, profileMats, genes, cluster = "group_A"){
-
+  
   meanProfile <- list()
   checkList <- c(attributes(profileMats[[1]])$dim, attributes(profileMats[[1]])$extend)
   
-
+  
   # rowId <- 1
   ## calculate mean profile for each matrix
   for (rowId in 1:nrow(exptInfo)) {
     mat <- profileMats[[ exptInfo$sampleId[rowId] ]]
-
+    
     ## make sure that all the matrices have same dimensions and extension
     if(! all(checkList == c(attributes(mat)$dim, attributes(mat)$extend))){
       stop("Error: matrix are of not same dimentions")
     }
-
+    
     mat <- mat[genes, ]
-
+    
     meanCol <- paste(exptInfo$sampleId[rowId], ".mean", sep = "")
     sdCol <- paste(exptInfo$sampleId[rowId], ".sd", sep = "")
     medianCol <- paste(exptInfo$sampleId[rowId], ".median", sep = "")
-
+    
     meanProfile[[ meanCol ]] <- apply(mat, MARGIN = 2, mean)
     meanProfile[[ sdCol ]] <- apply(mat, MARGIN = 2, sd)
     meanProfile[[ medianCol ]] <- apply(mat, MARGIN = 2, median)
-
+    
     cat("Calculated mean profile for sample", exptInfo$sampleId[rowId], "\n")
   }
-
+  
   meanProfileDf <- as_tibble(meanProfile, rownames = "bin")
-
+  
   meanProfileDf$bin <- c(attributes(profileMats[[1]])$upstream_index,
                          attributes(profileMats[[1]])$target_index,
                          attributes(profileMats[[1]])$downstream_index)
@@ -59,14 +59,14 @@ geneset_average_profile <- function(exptInfo, profileMats, genes, cluster = "gro
     endPos <- attributes(profileMats[[1]])$extend[2]
     binTotal <- attributes(profileMats[[1]])$dim[2]
     binWidth <- (endPos - startPos)/binTotal
-
+    
     posLabels <- tibble::tibble(
       breaks = c(meanProfileDf$bin[1], abs(startPos/binWidth), dplyr::last(meanProfileDf$bin[-1])),
       labels = c(startPos, attributes(profileMats[[1]])$target_name, endPos)
     )
-
+    
   } else if(! attributes(profileMats[[1]])$target_is_single_point){
-
+    
     
     posLabels <- tibble::tibble(
       breaks =  c(
@@ -93,11 +93,11 @@ geneset_average_profile <- function(exptInfo, profileMats, genes, cluster = "gro
     dplyr::mutate(
       sampleId = forcats::fct_relevel(.f = sampleId, !!!exptInfo$sampleId)
     )
-
+  
   plotDf$cluster <- cluster
   plotDf$groupSize <- length(genes)
   plotDf$groupLabel <- paste(cluster, ":", length(genes), "genes")
-
+  
   return(plotDf)
 }
 
@@ -125,10 +125,10 @@ draw_avg_profile_plot <- function(exptInfo,
                                   genes,
                                   lineColors = NULL,
                                   lineShape = NULL){
-
-
+  
+  
   plotDf <- geneset_average_profile(exptInfo = exptInfo, profileMats = profileMats, genes = genes)
-
+  
   ## decide the axis labels
   axisBrk <- NULL
   axisLab <- NULL
@@ -138,15 +138,15 @@ draw_avg_profile_plot <- function(exptInfo,
       attributes(profileMats[[1]])$target_index[1],
       tail(attributes(profileMats[[1]])$downstream_index, 1)
     )
-
+    
     axisLab <- c(
       -attributes(profileMats[[1]])$extend[1],
       attributes(profileMats[[1]])$target_name,
       attributes(profileMats[[1]])$extend[2]
     )
-
+    
     targetEnd <- axisBrk[2] + 1
-
+    
   } else if(! attributes(profileMats[[1]])$target_is_single_point){
     axisBrk <- c(
       attributes(profileMats[[1]])$upstream_index[1],
@@ -154,20 +154,23 @@ draw_avg_profile_plot <- function(exptInfo,
       tail(attributes(profileMats[[1]])$target_index, 1),
       tail(attributes(profileMats[[1]])$downstream_index, 1)
     )
-
+    
     axisLab <- c(
       -attributes(profileMats[[1]])$extend[1],
       "START", "END",
       attributes(profileMats[[1]])$extend[2]
     )
-
+    
     targetEnd <- axisBrk[3]
-
+    
   }
-
-  p <- ggplot2::ggplot(data = plotDf) +
-    geom_line(mapping = aes(x = bin, y = mean, group = sample, color = sample, linetype = sample),
-              size = 0.8, alpha = 0.8) +
+  
+  
+  pt <- ggplot2::ggplot(data = plotDf) +
+    geom_line(
+      mapping = aes(x = bin, y = mean, group = sampleId, color = sampleId, linetype = sampleId),
+      size = 0.8, alpha = 0.8
+    ) +
     geom_hline(yintercept = 0, size = 2, color = "grey70") +
     geom_segment(mapping = aes(x = axisBrk[2], y = 0, xend = targetEnd, yend = 0),
                  size = 10, lineend = "butt", color = "grey50") +
@@ -183,15 +186,16 @@ draw_avg_profile_plot <- function(exptInfo,
           axis.title.x = element_blank(),
           axis.title.y = element_text(face = "bold"),
           legend.position = c(1, 1),
+          panel.grid = element_blank(),
           legend.justification = c(1.1, 1.1),
           legend.text = element_text(size = 13),
           legend.title = element_blank()
     )
-
+  
   # p
-
-  return(p)
-
+  
+  return(pt)
+  
 }
 
 
